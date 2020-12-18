a9911a0223060375f1315219f9c55c23b151e2a4
euguroglu <e.uguroglu@yahoo.com>
2020-12-18 20:01:32 +0300

    sqllite example finished

--- app.py
+++ app.py
@@ -3,6 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///User.sqlite3'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	# Defines the Table Name user
+	__tablename__ = "user"
+
+	# Makes three columns into the table id, name, email
+	_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(100), nullable=False)
+	email = db.Column(db.String(100), nullable=False)
+
+	# A constructor function where we will pass the name and email of a user and it gets add as a new entry in the table.
+	def __init__(self, name, email):
+		self.name = name
+		self.email = email
 # Control will come here and then gets redirect to the index function
 @app.route("/")
 def home():
@@ -15,11 +33,17 @@ def index():
 		name = data["name"]
 		email = data["email"]
 
-		user_data = [{"name":name, "email":email}] # stores them into a dictionary
+		new_data = User(name, email)
+		db.session.add(new_data)
+		db.session.commit()
+
+		user_data = User.query.all()
+
 
 		return render_template("index.html", user_data = user_data) # passes user_data variable into the index.html file.
 
 	return render_template("index.html")
 
 if __name__=="__main__":
-	app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
